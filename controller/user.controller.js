48666d3af6814fc872067dd40d2e5d5bb2267a27
Shahzad <mech1090@gmail.com>
2022-06-02 20:50:44 -0600

    login done

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -6,7 +6,20 @@ const userService = require('../service/user.service')
 const getLoginForm = (req,res)=>{
     res.render('login/layout')
 }
-const login = (req,res)=>{}
+const login = async(req,res)=>{
+    const {email,password} = req.body
+    const findEntry = await userService.findUser({email})
+ //   const findEntry = await User.findOne({email})
+    if(!findEntry){
+        return res.render('signup/layout',{message:'User does not exist Signup please'})
+    }
+    const matchPassword = await bcrypt.compare(password,findEntry.password)
+    if(!matchPassword){
+        return res.render('login/layout',{message:'email or password wrong'})
+    }
+    return res.render('user/layout')
+}
+
 const getSignupForm = (req,res)=>{
     res.render('signup/layout')
 }
