cc22eaa786317e5599340afae8c6bf085a0652eb
Shahzad <mech1090@gmail.com>
2022-06-02 19:36:01 -0600

    model created and DB connected

--- db/index.js
+++ db/index.js
@@ -1,5 +1,5 @@
 const config = require('config')
 const mongoose = require('mongoose')
 const uri = config.get('database.uri') + '/' + config.get('database.name')
-const options = {useNewUrlParser:true,useUnifiedTopoloy:true}
+const options = {useNewUrlParser:true,useUnifiedTopology:true}
 module.exports = mongoose.connect(uri,options).catch(error=>console.log(error))
